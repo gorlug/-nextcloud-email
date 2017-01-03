a55ac4de0138b754847999ddcf3da0b1a6eaf71f
Achim Rohn <achim@rohn.eu>
2017-01-03 15:56:06 +0100

    Only execute a maintenance:install if there is no config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,10 @@ if [ ! -e '/var/www/html/version.php' ]; then
     tar cf - --one-file-system -C /usr/src/nextcloud . | tar xf -
     chown -R www-data /var/www/html
     chmod +x /var/www/html/occ
+fi
+if [ ! -e '/var/www/html/config/config.php' ]; then
+    echo "waiting for database connection"
+    sleep 30
     mysql -uroot -p${MYSQL_ROOT_PASSWORD} -hdb -e "create database if not exists nextcloud;"
     su www-data -c "/var/www/html/occ maintenance:install --database mysql --database-host db --database-name nextcloud --database-user root --database-pass \"${MYSQL_ROOT_PASSWORD}\" --admin-user \"${NEXTCLOUD_ADMIN_USER}\" --admin-pass \"${NEXTCLOUD_ADMIN_PASSWORD}\""
     su www-data -c "/var/www/html/occ config:system:set trusted_domains 0 --value ${CLOUD_HOSTNAME}"
